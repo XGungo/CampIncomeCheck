8ef4dc592cd3ef165cf993bd8ca10a3984f966ff
Jelly <daniel2000890311g@gmail.com>
2019-05-21 17:16:06 +0800

    test db 5

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -22,8 +22,8 @@ class BotsController < ApplicationController
     end
         
     def dual_content_check(channel_id, received_text)
-        unless AdminList.where(channel_id: channel_id).nil?
-            name = AdminList.where(channel_id: channel_id)[:name]
+        if AdminList.find(channel_id)
+            name = AdminList.find(channel_id).name
             p "#{name} is a admin"
         else
             admin_regist(channel_id,received_text)

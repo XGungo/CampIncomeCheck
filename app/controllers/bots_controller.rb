d296e2d653f66abe0ab3bc37cc9607c64d87354c
Jelly <daniel2000890311g@gmail.com>
2019-05-23 13:58:22 +0800

    move check to module

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -1,6 +1,7 @@
-
+require 'content_check'
 class BotsController < LineSettingsController
     protect_from_forgery with: :null_session
+    include ContentCheck
     def webhook()      
         case channel_status(source)
         when 'group', 'room'
@@ -12,41 +13,7 @@ class BotsController < LineSettingsController
         head :ok
     end
     
-    def dual_content_check(channel_id, received_text)
-        case dual_status(channel_id)
-        when :admin
-        when :user 
-        when :unregisted
-            admin_regist(channel_id,received_text)
-            user_regist(channel_id, received_text)
-        end 
-    end
-    def dual_status(channel_id)
-        if not AdminList.where(channel_id: channel_id).empty?
-            name = AdminList.where(channel_id: channel_id).last&.name
-            p "Admin, #{name}, has been check."
-            return :admin 
-        elsif not UserList.where(channel_id: channel_id).empty?
-            name = UserList.where(channel_id: channel_id).last&.name
-            p "User, #{name}, has been check."
-            return :user 
-        else
-            return :unregisted
-        end
-    end
-    def admin_regist(channel_id,received_text)
-        return nil unless received_text[0..5] == 'admin;'
-        name = received_text[6..-1]
-        AdminList.create(channel_id: channel_id, name: name)
-        p "#{name},have just registed Admin"
-    end
-    def user_regist(channel_id,received_text)
-        ini_score = 1000
-        return nil unless received_text[0..4] == 'user;'
-        name = received_text[5..-1]
-        UserList.create(channel_id: channel_id, name: name, dual_score: ini_score)
-        p "#{name},have just registed User"
-    end
+
     # merged_regist 
     # def regist(channel_id, received_text, dual_status)
     #     status = dual_status.to_s
---
+++ app/controllers/content_check.rb
@@ -0,0 +1,37 @@
+module ContentCheck
+    def dual_content_check(channel_id, received_text)
+        case dual_status(channel_id)
+        when :admin
+        when :user 
+        when :unregisted
+            admin_regist(channel_id,received_text)
+            user_regist(channel_id, received_text)
+        end 
+    end
+    def dual_status(channel_id)
+        if not AdminList.where(channel_id: channel_id).empty?
+            name = AdminList.where(channel_id: channel_id).last&.name
+            p "Admin, #{name}, has been check."
+            return :admin 
+        elsif not UserList.where(channel_id: channel_id).empty?
+            name = UserList.where(channel_id: channel_id).last&.name
+            p "User, #{name}, has been check."
+            return :user 
+        else
+            return :unregisted
+        end
+    end
+    def admin_regist(channel_id,received_text)
+        return nil unless received_text[0..5] == 'admin;'
+        name = received_text[6..-1]
+        AdminList.create(channel_id: channel_id, name: name)
+        p "#{name},have just registed Admin"
+    end
+    def user_regist(channel_id,received_text)
+        ini_score = 1000
+        return nil unless received_text[0..4] == 'user;'
+        name = received_text[5..-1]
+        UserList.create(channel_id: channel_id, name: name, dual_score: ini_score)
+        p "#{name},have just registed User"
+    end
+end
